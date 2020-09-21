47a67801aefa46aefe75e619e4cff74e07f09935
Gleydson Delcho <gleydson.engenhariacivil@gmail.com>
2020-09-21 16:15:57 -0300

    Altera o link do caminho da imagem back

--- frontend/src/components/template/Logo.jsx
+++ frontend/src/components/template/Logo.jsx
@@ -1,12 +1,13 @@
 import './Logo.css';
 import logo from '../../assets/images/logo.png';
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 export default props => 
     <aside className="logo">
-        <a href= '/' className="logo">
+        <Link to= '/' className="logo">
             <img src={ logo } alt="logo"/>
-        </a>        
+        </Link>        
         <h2>Está é a plataforma de acompanhamento de estudos Mentory</h2>
     </aside>
 
