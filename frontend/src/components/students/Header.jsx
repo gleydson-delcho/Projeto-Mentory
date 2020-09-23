3a93fc8e41fa1033b2910db38d29ab87365a1643
Gleydson Delcho <gleydson.engenhariacivil@gmail.com>
2020-09-23 11:55:57 -0300

    Modifica o html                                                                                                                                                                                                               Modifica o header e inclui novas divs                                                                                                                                                                                                                                                             Modifica o componente com a saída do form para um novo componente

--- frontend/src/components/students/Header.jsx
+++ frontend/src/components/students/Header.jsx
@@ -7,10 +7,15 @@ import { Link } from 'react-router-dom';
 export default props =>
     <header className="header">
         <div className="topbar-container">
-            <Link to="/">
-                <img src={ back } alt="back"/>
-            </Link>
-            <img src={ logo } alt="logo"/>
+            <div className="topbar">
+                <Link to="/">
+                    <img src={ back } classname="back" alt="back"/>
+                </Link>
+                <img src={ logo } classname="logo" alt="logo"/>
+            </div>
+            <div className="students-content">
+                <strong>Que incrível você procurar por nós!</strong>
+                <p>O primeiro passo será preencher este formulário de inscrição</p>
+            </div> 
         </div>
     </header>
-
--- frontend/src/components/students/Students.jsx
+++ frontend/src/components/students/Students.jsx
@@ -1,25 +1,17 @@
 import './Students.css';
 import React from 'react';
-import logo from '../../assets/images/logo.png';
-import back from '../../assets/images/back.svg'
-import { Link } from 'react-router-dom';
 
-// import Header from './Header';
-// import Content from './Content';
 
-export default props =>    
-        <div className="container">
-            <header className="header">
-                <div className="topbar-container">
-                    <Link to="/">
-                        <img src={ back } alt="back"/>
-                    </Link>
-                    <img src={ logo } alt="logo"/>
-                </div>
-            </header>
-            <div className="students-content">
-                <strong>Esses são os mentores disponíveis</strong>
-                
-            </div>
-        </div>
+
+import Header from './Header';
+import Forms from './Forms';
+
+export default props =>  
+    <React.Fragment>                   
+        <div className="container">   
+            <Header />  
+            <Forms />
+        </div>            
+    </React.Fragment>
+        
        
