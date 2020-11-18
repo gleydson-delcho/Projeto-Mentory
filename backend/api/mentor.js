f37ff0214baa489a50111bd24ece545ec3d656ab
Gleydson Delcho <gleydson.engenhariacivil@gmail.com>
2020-11-18 20:16:06 -0300

    Adiciona novos arquivos para conexão com o backend

---
+++ backend/api/mentor.js
@@ -0,0 +1,58 @@
+
+
+module.exports = app => {
+    const { existsOrError, notExistsOrError, equalsOrError, idValidated, emailValidated } = app.api.validation
+
+      const save = async (request, response) => {
+        const mentor = { ...request.body }
+        if(request.params.id) mentor.id = request.params.id
+
+        try {
+            existsOrError(mentor.name, 'Nome não foi informado!')
+            existsOrError(mentor.email, 'E-mail não foi informado!')
+            // emailValidated(mentor.email, 'O e-mail informado não é válido!')            
+            
+            const mentorFromDb = await app.db('mentors')
+                .where({ email: mentor.email }).first()
+
+            const existentMentor = !mentor.id
+
+            if(existentMentor){
+                notExistsOrError(mentorFromDb, 'Este usuário já foi cadastrado!')
+            }
+        }catch (msg) {
+            return response.status(400).send(msg)
+        }       
+
+        if(mentor.id) {
+            app.db('mentors')
+                .update(mentor)
+                .where({ id: mentor.id })
+                .then(_ => response.status(204).send())
+                .catch(err => response.status(500).send(err))
+        }else {
+            app.db('mentors')
+                .insert(mentor)
+                .then(_ => response.status(204).send())
+                .catch(err => response.status(500).send(err))
+        }
+    }
+
+    const getMentors = (request, response) => {
+        app.db('mentors')
+            .select('id', 'name', 'email', 'avatar', 'bio', 'whatsapp')
+            .then(mentors => response.json(mentors))
+            .catch(err => response.status(500).send(err))
+    }
+
+    const getMentorById = (request, response) => {
+        app.db('mentors')
+            .select('id', 'name', 'email', 'avatar', 'bio', 'whatsapp')
+            .where({id : request.params.id})
+            .first()
+            .then(mentor => response.json(mentor))
+            .catch(err => response.status(500).send(err))
+    }
+
+    return { save, getMentors, getMentorById };
+}
