c4a465d6084a66f0d533c6dc018a13344aa00b27
Gleydson Delcho <gleydson.engenhariacivil@gmail.com>
2020-11-21 21:39:45 -0300

    Ajusta a comunicação com o backend e retira funções comentadas que não serão utilizadas neste arquivo.

--- frontend/src/components/services/api.js
+++ frontend/src/components/services/api.js
@@ -1,28 +1,7 @@
 
 import axios from 'axios';
 
-   
-
-
-    const baseURL = 'http://localhost:3000'
-    
-    
-    function getMentors() {
-        const mentor = this.state.mentor;
-        const url = mentor.id ? `${baseURL}/mentors/${mentor.id}` : `${baseURL}/mentors`;
-        
-        axios.get(url, mentor)
-            .then(resp => {
-                const list = this.getUpdatedList(resp.data);
-                this.setState({user: mentor, list});
-            })
-    }
-
-    function getUpdatedList (mentor, add = true) {
-        const list = this.state.list.filter(m => m.id !== mentor.id);
-        if(add) list.unshift(mentor);
-        return list;
-    }
-
-
-export default { getMentors, getUpdatedList }
+const api = axios.create({
+    baseURL: 'http://localhost:3000'
+})
+export default api
